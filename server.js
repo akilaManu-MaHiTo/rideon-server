18c0c3005de63c91dc4b51856954eb05024eec76
akilaManu <akilamanujith@gmail.com>
2025-09-02 17:07:01 +0530

    Add express.urlencoded middleware for enhanced request parsing

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: `${process.env.SERVER_ORIGIN}`,
     methods: ["POST", "GET", "PUT", "DELETE"],
