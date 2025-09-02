2afb7a52f8e554d46c0371fed0f16fce547a8742
akilaManu <akilamanujith@gmail.com>
2025-09-02 17:16:00 +0530

    Refactor payment notification handler by removing MD5 signature verification

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,44 +1,21 @@
-const crypto = require('crypto');
-const Payment = require('../models/Checkout');
-
-const verifyMd5Sig = (body, merchant_secret) => {
-  const {
-    merchant_id,
-    order_id,
-    payhere_amount,
-    payhere_currency,
-    status_code,
-  } = body;
-
-  
-  const str = `${merchant_id}${order_id}${merchant_secret}${payhere_amount}${payhere_currency}${status_code}`;
-  const hash = crypto.createHash('md5').update(str).digest('hex');
-
-  return hash === body.md5sig;
-};
+const Payment = require("../models/Checkout");
 
 exports.notifyPayment = async (req, res) => {
   try {
     const paymentData = req.body;
 
-    const MERCHANT_SECRET = process.env.PAYHERE_SECRET;
-
-    if (!verifyMd5Sig(paymentData, MERCHANT_SECRET)) {
-      console.log('MD5 signature mismatch!');
-      return res.status(400).send('Invalid signature');
-    }
-
+    // Save or update payment
     const payment = await Payment.findOneAndUpdate(
       { payment_id: paymentData.payment_id },
       paymentData,
       { upsert: true, new: true, setDefaultsOnInsert: true }
     );
 
-    console.log('Payment processed:', payment.payment_id);
+    console.log("Payment processed:", payment.payment_id);
 
-    res.status(200).send('OK');
+    res.status(200).send("OK");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
