0c533900514222c8fd3639bacc846be3837e1707
akilaManu <akilamanujith@gmail.com>
2025-10-06 21:17:21 +0530

    Add bikeCount and addedBikesArray fields to BikeStation model; update create and update methods to handle new fields

--- controllers/bikeStationController.js
+++ controllers/bikeStationController.js
@@ -2,7 +2,14 @@ const BikeStation = require("../models/BikeStation");
 
 exports.createBikeStation = async (req, res) => {
   try {
-    const { stationName, stationLocation, latitude, longitude } = req.body;
+    const {
+      stationName,
+      stationLocation,
+      latitude,
+      longitude,
+      bikeCount,
+      addedBikesArray,
+    } = req.body;
 
     const firstLetter = stationLocation.charAt(0).toUpperCase();
     const thirdLetter =
@@ -16,10 +23,18 @@ exports.createBikeStation = async (req, res) => {
     const randomNum = Math.floor(Math.random() * 10000);
     const stationId = `${firstLetter}${thirdLetter}${lastLetter}-${randomNum}`;
 
+    if (bikeCount < addedBikesArray.length) {
+      return res.status(400).json({
+        message: "Bike count does not match the number of bikes provided",
+      });
+    }
+
     const bikeStation = await BikeStation.create({
       stationId,
       stationName,
       stationLocation,
+      bikeCount,
+      bikes: addedBikesArray,
       latitude,
       longitude,
     });
@@ -34,7 +49,14 @@ exports.createBikeStation = async (req, res) => {
 exports.updateBikeStation = async (req, res) => {
   try {
     const { id } = req.params;
-    const { stationName, stationLocation, latitude, longitude } = req.body;
+    const {
+      stationName,
+      stationLocation,
+      latitude,
+      longitude,
+      bikeCount,
+      addedBikesArray,
+    } = req.body;
 
     const firstLetter = stationLocation.charAt(0).toUpperCase();
     const thirdLetter =
@@ -44,13 +66,19 @@ exports.updateBikeStation = async (req, res) => {
     const lastLetter = stationLocation
       .charAt(stationLocation.length - 1)
       .toUpperCase();
-
+    if (bikeCount < addedBikesArray.length) {
+      return res.status(400).json({
+        message: "Bike count does not match the number of bikes provided",
+      });
+    }
     const randomNum = Math.floor(Math.random() * 10000);
     const stationId = `${firstLetter}${thirdLetter}${lastLetter}-${randomNum}`;
     const updatedStation = await BikeStation.findByIdAndUpdate(id, {
       stationId,
       stationName,
       stationLocation,
+      bikeCount,
+      bikes: addedBikesArray,
       latitude,
       longitude,
     });
@@ -70,7 +98,7 @@ exports.deleteBikeStation = async (req, res) => {
   try {
     const { id } = req.params;
     const bikeStations = await BikeStation.findByIdAndDelete(id);
-    res.status(200).json({message:"Bike Station Delete Successfully"});
+    res.status(200).json({ message: "Bike Station Delete Successfully" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
@@ -79,7 +107,7 @@ exports.deleteBikeStation = async (req, res) => {
 
 exports.getAllBikeStation = async (req, res) => {
   try {
-    const bikeStations = await BikeStation.find();
+    const bikeStations = await BikeStation.find().populate("bikes");
     res.status(200).json(bikeStations);
   } catch (err) {
     console.error(err);
