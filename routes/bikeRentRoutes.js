fe73a640fb09261129c17dae6dd64ba3ef5a1713
akilaManu <akilamanujith@gmail.com>
2025-10-09 12:09:32 +0530

    fix: import authMiddleware in bike rental routes

--- routes/bikeRentRoutes.js
+++ routes/bikeRentRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { rentBikeCreate } = require("../controllers/bikeRentController");
+const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/", protect, rentBikeCreate);
