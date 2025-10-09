73c3822c8b6d65c92eefc6dc2b4156d626196480
Supun Liyanage <174965581+SupunLiyanage88@users.noreply.github.com>
2025-10-09 22:10:34 +0530

    fix: correct default value casing for role field in user schema

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
     role:{
       type: String,
       enum: ['Admin', 'User'],
-      default: 'user'
+      default: 'User'
     },
     rc: { 
       type: Number, 
