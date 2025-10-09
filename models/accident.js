265b2bdc23109c3fb1b56338a971d9119f29036d
Buddhila <buddhilalakshan12@gmail.com>
2025-10-10 01:49:46 +0530

    f

--- models/accident.js
+++ models/accident.js
@@ -6,7 +6,6 @@ const accidentSchema = new mongoose.Schema(
     latitude: { type: Number, required: true },
     longitude: { type: Number, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    isRented: {type: Boolean, default: true, required: true },
   },
   { timestamps: true }
 );
