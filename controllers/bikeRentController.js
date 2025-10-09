d0dc0d4d2641a6dec443a3fce6423218eb83507d
akilaManu <akilamanujith@gmail.com>
2025-10-09 13:34:12 +0530

    fix: remove expiresAt field from rentBikeCreate function and enhance user validation

--- controllers/bikeRentController.js
+++ controllers/bikeRentController.js
@@ -1,14 +1,30 @@
 const RentBike = require("../models/RentBike");
-
+const User = require("../models/User");
 exports.rentBikeCreate = async (req, res) => {
   try {
-    const { bikeId, distance, duration, rcPrice, expiresAt,selectedStationId } = req.body;
+    const { bikeId, distance, duration, rcPrice, selectedStationId } = req.body;
     const id = req.user.id;
+
     if (!bikeId || !distance || !duration || !rcPrice || !selectedStationId) {
       return res
         .status(400)
         .json({ message: "All required fields must be provided" });
     }
+
+    const findUser = await User.findById(id);
+    if (!findUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (findUser.rc < rcPrice) {
+      return res
+        .status(400)
+        .json({ message: "Insufficient RideOn Coins (RC)" });
+    }
+    const user = await User.findByIdAndUpdate(
+      id,
+      { $inc: { rc: -rcPrice } },
+      { new: true }
+    );
 
     const rentBike = new RentBike({
       bikeId,
@@ -16,12 +32,13 @@ exports.rentBikeCreate = async (req, res) => {
       duration,
       rcPrice,
       userId: id,
-      selectedStationId
+      selectedStationId,
     });
 
     const savedRentBike = await rentBike.save();
     res.status(201).json(savedRentBike);
   } catch (error) {
+    console.error(error);
     res
       .status(500)
       .json({ message: "Error creating rent bike", error: error.message });
